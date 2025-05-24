886b9c6ed45ae0b5a53f08c1c2d952c298900c27
danisyam095 <danisyamsudin095@gmail.com>
2025-05-24 14:41:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,31 @@ if ('serviceWorker' in navigator) {
       .catch(err => console.error('Service Worker registration failed:', err));
   });
 }
+
+let deferredPrompt;
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Simpan event untuk trigger nanti
+  e.preventDefault();
+  deferredPrompt = e;
+
+  // Tampilkan tombol install
+  installBtn.hidden = false;
+
+  installBtn.addEventListener('click', () => {
+    // Tampilkan prompt
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+
+    installBtn.hidden = true;
+  });
+});
