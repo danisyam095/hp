71052df773afdd4f9710ade37d198dc83e5706b7
danisyam095 <danisyamsudin095@gmail.com>
2025-05-24 14:36:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/my-pwa/service-worker.js')
+    navigator.serviceWorker.register('/hp/service-worker.js')
       .then(reg => console.log('Service Worker registered:', reg))
       .catch(err => console.error('Service Worker registration failed:', err));
   });
