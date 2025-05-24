b632f18d70f230cfaf4a503f2d034abe2fefd008
danisyam095 <danisyamsudin095@gmail.com>
2025-05-24 14:32:14 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  '/my-pwa/',
+  '/my-pwa/index.html',
+  '/my-pwa/style.css',
+  '/my-pwa/app.js',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
